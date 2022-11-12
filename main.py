1419631b2cba02ac05ca65fbb4bc024ebd80226c
haoching <89097803+haoching@users.noreply.github.com>
2022-11-12 15:26:25 +0800

    test func

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from nextcord.ext import commands
 from os import environ
 from dotenv import load_dotenv
 import auth
+import json
 
 load_dotenv()
 
@@ -10,10 +11,17 @@ token = environ["TOKEN"]
 
 TESTING_GUILD_ID = 872415885321732117  # Replace with your guild ID
 
-a = auth.Auth(auth.app_id, auth.app_key)
-auth_response = auth.requests.post(auth.auth_url, a.get_auth_header())
-d = auth.data(auth.app_id, auth.app_key, auth_response)
-data_response = auth.requests.get(auth.url, headers=d.get_data_header())
+#GetBusInfo(url) return response
+def GetBusInfo(url):
+    a = auth.Auth(auth.app_id, auth.app_key)
+    auth_response = auth.requests.post(auth.auth_url, a.get_auth_header())
+    d = auth.data(auth.app_id, auth.app_key, auth_response)
+    data_response = auth.requests.get(url, headers=d.get_data_header())
+    return data_response.text
+path = 'temp.json'
+with open(path, 'w') as f:
+    f.write(GetBusInfo('https://tdx.transportdata.tw/api/basic/v2/Rail/TRA/LiveTrainDelay?$top=30&$format=JSON'))
+
 
 bot = commands.Bot()
 
@@ -23,7 +31,7 @@ async def on_ready():
 
 @bot.slash_command(description="My first slash command", guild_ids=[TESTING_GUILD_ID])
 async def bus(interaction: nextcord.Interaction):
-    await interaction.send(data_response.text)
+    await interaction.send()
 
 
 bot.run(token)
