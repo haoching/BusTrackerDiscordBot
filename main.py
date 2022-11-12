171baf9bdb289d1786bc889ecd0a855abedf46e4
haoching <89097803+haoching@users.noreply.github.com>
2022-11-12 13:29:10 +0800

    test

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import nextcord
 from nextcord.ext import commands
 from os import environ
 from dotenv import load_dotenv
+import auth
 
 load_dotenv()
 
 token = environ["TOKEN"]
 
 TESTING_GUILD_ID = 872415885321732117  # Replace with your guild ID
+
+a = auth.Auth(auth.app_id, auth.app_key)
+auth_response = auth.requests.post(auth.auth_url, a.get_auth_header())
+d = auth.data(auth.app_id, auth.app_key, auth_response)
+data_response = auth.requests.get(auth.url, headers=d.get_data_header())
 
 bot = commands.Bot()
 
@@ -16,8 +22,8 @@ async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 @bot.slash_command(description="My first slash command", guild_ids=[TESTING_GUILD_ID])
-async def hello(interaction: nextcord.Interaction):
-    await interaction.send("Hello!")
+async def bus(interaction: nextcord.Interaction):
+    await interaction.send(data_response.text)
 
 
 bot.run(token)
