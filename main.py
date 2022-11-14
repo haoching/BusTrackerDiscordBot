187b9bac35899d6fab41ec4715d11b7d2e6ad6a6
haoching <haoching929@gmail.com>
2022-11-14 11:52:13 +0800

    chang url

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def GetBusInfo(url):
     return data_response.text
 path = 'temp.json'
 with open(path, 'w') as f:
-    json.dump(GetBusInfo('https://tdx.transportdata.tw/api/basic/v2/Rail/TRA/LiveTrainDelay?$top=30&$format=JSON'), f)
+    json.dump(GetBusInfo('https://tdx.transportdata.tw/api/basic/v2/Bus/RealTimeByFrequency/City/Taichung?%24top=30&%24format=JSON'), f)
 
 bot = commands.Bot()
 
